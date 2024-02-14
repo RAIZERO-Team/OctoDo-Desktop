85db6dcd71c6728fe13b82ffa6e4545cbf79cbe4
Mohamed <mohammedelkady8889@gmail.com>
2024-02-15 01:50:01 +0200

    Preparing work in Database (sign up / sign in)

--- OctoDo/src/Backend/Account/User.java
+++ OctoDo/src/Backend/Account/User.java
@@ -1,39 +1,45 @@
-
 package Backend.Account;
 
-
 public class User {
-    
-    
-       private String  userName;
+
+    public static User currentUser;
+    private String userName;
     private String userEmail;
-    private String userPassword; 
+    private String userPassword;
     private String userType;
-    private String userImage; 
+    private String userImage;
     private String userGender;
-     
-    public String gitUserName(){
+
+    public String gitUserName() {
         return userName;
     }
-    public String gitUserEmail(){
+
+    public String gitUserEmail() {
         return userEmail;
-    } 
-    
-   public String gitUserType(){
+    }
+
+    public String gitUserType() {
         return userType;
-    } 
- public String gitUserImage(){
+    }
+
+    public String gitUserImage() {
         return userImage;
-    } 
-  public String gitUserGender(){
+    }
+
+    public String gitUserGender() {
         return userGender;
-    } 
-  public User (String  userName,String userEmail, String userPassword,  String userType, String userImage, String userGender){
-     this.userName=userName;
-     this.userEmail=userEmail;
-     this.userPassword=userPassword;
-     this.userType=userType;
-     this.userImage=userImage;
-     this.userGender=userGender;
-  }
+    }
+
+    public User(String userName, String userEmail, String userPassword, String userType, String userImage, String userGender) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userType = userType;
+        this.userImage = userImage;
+        this.userGender = userGender;
+    }
+
+    public User() {
+    }
+
 }
--- OctoDo/src/Backend/Database/ConnectionDB.java
+++ OctoDo/src/Backend/Database/ConnectionDB.java
@@ -1,7 +1,24 @@
-
 package Backend.Database;
 
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import raven.toast.Notifications;
 
 public class ConnectionDB {
-    
+
+    static Connection con;
+
+    static {
+        try {
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/Octodo",
+                    "root", "Mkh@3032024M");
+
+        } catch (SQLException ex) {
+            Notifications.getInstance().show(Notifications.Type.ERROR, "Error");
+
+        }
+    }
+
 }
