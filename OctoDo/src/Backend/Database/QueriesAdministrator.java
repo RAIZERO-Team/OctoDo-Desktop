c382eb8c3b27f58880a88896b2044e6743097de3
Mohamed <mohammedelkady8889@gmail.com>
2024-02-19 04:49:21 +0200

    complete sign in method in class QueriesAdministrator

--- OctoDo/src/Backend/Database/QueriesAdministrator.java
+++ OctoDo/src/Backend/Database/QueriesAdministrator.java
@@ -1,6 +1,7 @@
 package Backend.Database;
 
 import Backend.Account.User;
+import static Backend.Database.ConnectionDB.con;
 import java.sql.*;
 
 public class QueriesAdministrator {
@@ -19,28 +20,43 @@ public class QueriesAdministrator {
     }
 
     public static boolean sign_in(String Email, String paswword) {
-        return false;
+        try {
+            stm = con.prepareStatement("select count(*) from User where User_email = ? and User_password = ? ");
+            stm.setString(1, Email);
+            stm.setString(2, encryptionPassword(paswword));
+
+            ResultSet re = stm.executeQuery();
+            int count = 0;
+            while (re.next()) {
+                count = re.getInt(1);
+            }
+            return count >= 1;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
     }
 
-    public static User CurrentUser(String email)  throws SQLException{
-        
+    public static User CurrentUser(String email) throws SQLException {
+
         stm = ConnectionDB.con.prepareStatement("select User_fname , User_lname ,User_ gender from user where User_email=? ");
         stm.setNString(1, email);
         ResultSet rs = stm.executeQuery();
         if (rs.next()) {
-        String user_fname = rs.getString("User_fname");
-        String user_lname = rs.getString("User_lname");
-        String user_gender = rs.getString("User_gender");
-     
-        return new User( user_fname, user_lname, user_gender);
-        } 
-        
-    
+            String user_fname = rs.getString("User_fname");
+            String user_lname = rs.getString("User_lname");
+            String user_gender = rs.getString("User_gender");
+
+            return new User(user_fname, user_lname, user_gender);
+        }
+
         return null;
-        
+
     }
 
-    
     static private String encryptionPassword(String password) {
 
         return password;
@@ -50,14 +66,16 @@ public class QueriesAdministrator {
 
         return password;
     }
-    
-    public static boolean updatePassword(String username , String newPassword) {
+
+    public static boolean updatePassword(String username, String newPassword) {
         try {
-            stm = ConnectionDB.con.prepareStatement("UPDATE User SET user_password = ? WHERE user_email = ?");
-            stm.setString(1 , username);
-            stm.setString(2 , newPassword);
-            return true ;
-        }catch (SQLException e) {
+            stm = ConnectionDB.con.prepareStatement("UPDATE User SET User_password = ? WHERE User_email = ?");
+
+            stm.setString(1, username);
+            stm.setString(2, newPassword);
+            stm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
             return false;
         }
     }
