e6c3856f657ec9725125a83dc7f818dff1d6d1af
Omniazedan <157979341+Omniazedan@users.noreply.github.com>
2024-03-05 01:12:13 -0800

    update QuerieAdninstratorSqlite

--- OctoDo/src/Backend/Database/QuerieAdninstratorSqlite.java
+++ OctoDo/src/Backend/Database/QuerieAdninstratorSqlite.java
@@ -2,6 +2,7 @@
 package Backend.Database;
 
 
+import static Backend.Database.ConnectionDB.con;
 import static Backend.Database.ConnectionDB.sqlitecon;
 import com.sun.jdi.connect.spi.Connection;
 
@@ -37,6 +38,39 @@ public class QuerieAdninstratorSqlite {
              Notifications.getInstance().show(Notifications.Type.ERROR, "Error");
         }
     }
-      
+      static private PreparedStatement stm;
+     public static boolean InsertData( String Medicine_Name ,  String Reminder_Times,String Dosage_ParDay,String Medicine_Time,String Days ,String Duration){ 
+     try {   
+ stm = con.prepareStatement( "INSERT INTO Medicine (Medicine_name, reminderTimes,Dosage_ParDay, Medicine_Time, Days , Duration) VALUES (?, ?, ?, ?, ?, ?)");
+    stm.setString(1,Medicine_Name);
+    stm.setString(2,  Reminder_Times);
+    stm.setString(3, Dosage_ParDay);
+     stm.setString(4,  Medicine_Time); 
+     stm.setString(5, Days);
+     stm.setString(6, Duration);
+  
+      stm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
 
-}
+            return false;
+        }
+   
+
+     }
+ public static boolean DeleteData( String MedicineName){
+      try{
+        stm = con.prepareStatement("DELETE FROM Medicine WHERE Medicine_Name =?");
+         stm.setString(1,MedicineName);
+         stm.executeUpdate();
+          return true;
+        } catch (SQLException e) {
+            return false;
+        } 
+          
+          
+      }  } 
+
+
+
+
